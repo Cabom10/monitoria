667a60b20496b692e030088d22500f16dda25144
Hugo Plantz <hmplantz@gmail.com>
2024-03-28 13:13:35 -0300

    duvida

---
+++ Untitled-1.go
@@ -0,0 +1,14 @@
+inteiro: vetor[100]
+k=?
+
+leia vetor, leia k
+para i de 0 até 99{ faça
+x= vetor[i]
+enquanto x!=k{
+    i=i+1
+    se i>=100
+    imrpima("valor não encontrado!)
+    se x=k
+}
+imprima(i)
+}
