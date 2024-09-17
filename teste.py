5382ac3ee48b6fc238ab885c3661c60a63d939d0
Hugo Plantz <hmplantz@gmail.com>
2024-09-17 15:45:19 -0300

    monitoria

--- teste.py
+++ teste.py
@@ -1,29 +1,21 @@
-# Definindo a estrutura de registro
-class Registro:
-    def __init__(self, valor, peso):
-        self.valor = valor
-        self.peso = peso
+def contar_digitos(P):
+    total_digitos=0
+    atual=1
+    while atual <=P:
+        proximo=atual*10
+        limiteSup=min(P,proximo-1)
 
-# Criando a matriz de registros
-matriz = [
-    [Registro(2, 1), Registro(4, 3)],
-    [Registro(6, 2), Registro(1, 5)],
-    [Registro(3, 4), Registro(5, 6)]
-]
+        total_folhas=limiteSup-atual+1
 
-# Inicializando o vetor e a soma
-vetor = [0, 0]
-soma = 0
+        total_digitos+= total_folhas * len(str(atual)) 
 
-# Preenchendo o vetor com base na matriz
-for i in range(3):
-    for j in range(2):
-        vetor[j] += matriz[i][j].valor * matriz[j][j % 2].peso
+        atual=proximo  
+    return total_digitos
 
-# Calculando a soma condicional
-for k in range(2):
-    if vetor[k] > 15:
-        soma += vetor[k] * (k + 2)
 
-# Exibindo o resultado
-print(soma)
+
+
+P=int(input("Digite a quantidade de paginas: "))   
+
+print("Total digitos: ",contar_digitos(P))
+
